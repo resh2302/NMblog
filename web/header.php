a902b3bd92ca209a5b82ff31800f1399bd64ffd1
Reshma <resh2302@gmail.com>
2014-05-24 22:52:38 -0400

    home page

---
+++ web/header.php
@@ -0,0 +1,13 @@
+<html>
+<head>
+	<title>Night Market Blog</title>
+	<link rel="stylesheet" type="text/css" href="css/normalize.css">
+	<link rel="stylesheet" type="text/css" href="css/foundation/foundation.min.css">
+	<link rel="stylesheet" type="text/css" href="css/main.css">
+</head>
+<body>
+	<div class="row">
+      <header class="large-12 columns">
+        <h1>Night Market Blog</h1>
+      </header>
+    </div>
---
+++ web/index.php
@@ -0,0 +1,90 @@
+<?php include 'header.php' ?>
+
+    <div id="home-content">
+    	
+    	<div class="row">
+            <div class="hex-content small-12  large-7 large-centered medium-8 medium-centered columns">
+                <div class="hex-wrapper">
+                    <div class="hex-row odd">
+                        <a href="#">
+                            <div class="hex" id="hex1">
+                                <div class="top"></div>
+                                <div class="middle"></div>
+                                <div class="bottom"></div>
+                            </div>
+                        </a>
+                        <a href="#">
+                            <div class="hex" id="hex2">
+                                <div class="top"></div>
+                                <div class="middle">
+                                    <img src="images/marki.png">
+                                </div>
+                                <div class="bottom"></div>
+                            </div>
+                        </a>
+                        <a href="#">
+                            <div class="hex" id="hex3">
+                                <div class="top"></div>
+                                <div class="middle"></div>
+                                <div class="bottom"></div>
+                            </div>
+                        </a>
+                    </div>
+                    <div class="hex-row even">
+                        <a href="#">
+                            <div class="hex" id="hex4">
+                                <div class="top"></div>
+                                <div class="middle">
+                                    <img src="images/reshma.png">
+                                </div>
+                                <div class="bottom"></div>
+                            </div>
+                        </a>
+                        <a href="#">
+                            <div class="hex" id="hex5">
+                                <div class="top"></div>
+                                <div class="middle"></div>
+                                <div class="bottom"></div>
+                            </div>
+                        </a>
+                        <a href="#">
+                            <div class="hex" id="hex6">
+                                <div class="top"></div>
+                                <div class="middle">
+                                    <img src="images/shannon.png">
+                                </div>
+                                <div class="bottom"></div>
+                            </div>
+                        </a>
+                    </div>
+                    <div class="hex-row odd">
+                        <a href="#">
+                            <div class="hex" id="hex7">
+                                <div class="top"></div>
+                                <div class="middle"></div>
+                                <div class="bottom"></div>
+                            </div>
+                        </a>
+                        <a href="#">
+                            <div class="hex" id="hex8">
+                                <div class="top"></div>
+                                <div class="middle">
+                                    <img src="images/jackie.png">
+                                </div>
+                                <div class="bottom"></div>
+                            </div>
+                        </a>
+                        <a href="#">
+                            <div class="hex" id="hex9">
+                                <div class="top"></div>
+                                <div class="middle"></div>
+                                <div class="bottom"></div>
+                            </div>
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+<?php include 'footer.php' ?>
