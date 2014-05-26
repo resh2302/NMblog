5d0c0fa0b8d9ee4bff3df028945a15264914ddfd
Reshma <resh2302@gmail.com>
2014-05-26 11:07:14 -0400

    Reshma - home page done responsive remaining

--- web/footer.php
+++ web/footer.php
@@ -1,8 +1,7 @@
 
 	<script src="js/vendor/jquery.js"></script>
     <script src="js/foundation.min.js"></script>
+    <script type="text/javascript" src="js/jquery.contenthover.js"></script>
     <script>
       $(document).foundation();
     </script>
-</body>
-</html>
--- web/index.php
+++ web/index.php
@@ -1,90 +1,105 @@
 <?php include 'header.php' ?>
 
-    <div id="home-content">
-    	
+    <div id="home-content">	
     	<div class="row">
-            <div class="hex-content small-12  large-7 large-centered medium-8 medium-centered columns">
-                <div class="hex-wrapper">
+            <div class="hex-content small-12  large-7 large-centered medium-9 medium-centered columns">
+                
                     <div class="hex-row odd">
-                        <a href="#">
-                            <div class="hex" id="hex1">
-                                <div class="top"></div>
-                                <div class="middle"></div>
-                                <div class="bottom"></div>
-                            </div>
-                        </a>
-                        <a href="#">
-                            <div class="hex" id="hex2">
-                                <div class="top"></div>
-                                <div class="middle">
-                                    <img src="images/marki.png">
-                                </div>
-                                <div class="bottom"></div>
-                            </div>
-                        </a>
-                        <a href="#">
-                            <div class="hex" id="hex3">
-                                <div class="top"></div>
-                                <div class="middle"></div>
-                                <div class="bottom"></div>
-                            </div>
-                        </a>
+                        <div class="hex" id="hex2">
+                            <a href="#" id="markiImg">
+                                <img src="images/marki2.png">
+                            </a>
+                        </div>
+                        <div class="contenthover">
+                            <a href="http://www.google.com">
+                                <img src="images/marki-hover.png">
+                            </a>
+                        </div>
+                        
+                        <div class="hex" id="hex3">
+                            <a href="#">
+                                <img src="images/jackie2.png">
+                            </a>
+                        </div>  
+                        <div class="contenthover">
+                            <a href="#">
+                                <img src="images/marki-hover.png">
+                            </a>
+                        </div>                      
                     </div>
                     <div class="hex-row even">
-                        <a href="#">
-                            <div class="hex" id="hex4">
-                                <div class="top"></div>
-                                <div class="middle">
-                                    <img src="images/reshma.png">
-                                </div>
-                                <div class="bottom"></div>
-                            </div>
-                        </a>
-                        <a href="#">
-                            <div class="hex" id="hex5">
-                                <div class="top"></div>
-                                <div class="middle"></div>
-                                <div class="bottom"></div>
-                            </div>
-                        </a>
-                        <a href="#">
-                            <div class="hex" id="hex6">
-                                <div class="top"></div>
-                                <div class="middle">
-                                    <img src="images/shannon.png">
-                                </div>
-                                <div class="bottom"></div>
-                            </div>
-                        </a>
+                        <div class="hex" id="hex4">
+                            <a href="#">
+                                <img src="images/shannon2.png">
+                            </a>
+                        </div>
+                        <div class="contenthover">
+                            <a href="#">
+                                <img src="images/marki-hover.png">
+                            </a>
+                        </div>
+                        <div class="hex" id="hex5">
+                            <a href="#">
+                                <img src="images/centerhex.png">
+                            </a>
+                        </div>
+                        <div class="contenthover">
+                            <a href="#">
+                                <img src="images/marki-hover.png">
+                            </a>
+                        </div>
+                        
+                        <div class="hex" id="hex6">
+                            <a href="#">
+                                <img src="images/kate2.png">
+                            </a>                                
+                        </div>
+                        
+                        <div class="contenthover">
+                            <a href="#">
+                                <img src="images/marki-hover.png">
+                            </a>
+                        </div>
                     </div>
                     <div class="hex-row odd">
-                        <a href="#">
-                            <div class="hex" id="hex7">
-                                <div class="top"></div>
-                                <div class="middle"></div>
-                                <div class="bottom"></div>
-                            </div>
-                        </a>
-                        <a href="#">
-                            <div class="hex" id="hex8">
-                                <div class="top"></div>
-                                <div class="middle">
-                                    <img src="images/jackie.png">
-                                </div>
-                                <div class="bottom"></div>
-                            </div>
-                        </a>
-                        <a href="#">
-                            <div class="hex" id="hex9">
-                                <div class="top"></div>
-                                <div class="middle"></div>
-                                <div class="bottom"></div>
-                            </div>
-                        </a>
+                        <div class="hex" id="hex7">
+                            <a href="#">
+                                <img src="images/prabhjot2.png">                                
+                            </a>
+                        </div>
+                        
+                        <div class="contenthover">
+                            <a href="#">
+                                <img src="images/marki-hover.png">
+                            </a>
+                        </div>
+                        
+                        <div class="hex" id="hex8">
+                            <a href="#">
+                                <img src="images/reshma2.png">     
+                            </a>                           
+                        </div>
+                        
+                        <div class="contenthover">
+                            <a href="#">
+                                <img src="images/marki-hover.png">
+                            </a>
+                        </div>
                     </div>
-                </div>
+                
             </div>
         </div>
     </div>
 
 <?php include 'footer.php' ?>
+
+    <script type="text/javascript">
+        $('.hex').contenthover({
+            width: 0,
+            height: 182,
+            overlay_opacity:0.8,
+            overlay_width: 0
+        });
+    </script>
+</body>
+</html>
