8cf12354252701072b8fee78d37d15fc03a1f9d4
Marki Sveen <marki.sveen@gmail.com>
2014-05-27 23:22:26 -0400

    Marki - blog

--- web/kate.php
+++ web/kate.php
@@ -1,76 +1,95 @@
-<?php
+<?php include "header.php"; ?>
 
-include "header.php";
-
-?>
 <div class="wrapper">
+
     <div class="row">
     <div class="large-12 columns blgcontent">
-        <div class="black-ribbon">
-<!--            <img class="bookmark" src="images/bookmark.png" />-->
+        
+        <div class="row">  
+          <div class="small-12 black-ribbon">
+              <a href="index.php"><img class="bookmark" src="images/bookmark.png" /></a>
+          </div>
         </div>
+        
         <div class="row">
             <div class="small-8 small-centered columns blog-head">
-                <div class="row">
-                    <div class="small-10 columns line">
-                        <h1>THE AMSTERDAM BREWERY</h1>
-                        <label class="by">By</label><label class="name"> KATE</label>
+
+                <div class="row">                    
+                    <div class="small-12 medium-10 columns line">
+                        <h1>The Amsterdam Brewery</h1>
+                        
+                        <p><span class="by">By</span> <span class="name">Kate Gollogly</span></p>
+
                         <div class="divline"></div>
-                        <div class="row">
-                            <div class="medium-4 columns date-cont"><img src="images/clock.png" /><label class="date"> May 26, 2014</label></div>
-                            <div class="medium-4 columns comments-cont"><img src="images/comments.png" /><label class="comments"></label></div>
-                            <div class="medium-4 columns"></div>
+                        
+                        <div class="row">                         
+                            <div class="medium-4 columns date-cont">
+                                <img src="images/clock.png" />
+                                <span class="date"> May 26, 2014</span>
+                            </div>
+                            
+                            <div class="medium-4 columns comments-cont">
+                                <img src="images/comments.png" />
+                                <label class="comments"></label>
+                            </div>
+
                             <div class="medium-4 columns"></div>
                         </div>
-                        
                     </div>
-                    <div class="small-2 columns auth-img">
-                        
-                        <img src="images/kate2.png" height="100" width="100">
-                        
+
+                    <div class="small-12 medium-2 columns auth-img">                      
+                        <img src="images/kate2.png">
                     </div>
                 </div>
-                 
 
             </div>
         </div>
+
         <div class="row">
             <div class="small-8 small-centered columns blog-content">
-                <p>Gentrify street art jean shorts Bushwick. Marfa VHS paleo, wayfarers scenester gentrify organic Helvetica chillwave tote bag sriracha Pitchfork Kickstarter food truck 8-bit. Yr raw denim sartorial, banh mi chambray Kickstarter street art roof party Cosby sweater pork belly gluten-free cornhole church-key. Semiotics Portland freegan farm-to-table, gluten-free wayfarers vegan. Wolf Carles sustainable vinyl. Church-key tousled chia PBR&B Portland cliche. Carles butcher yr tattooed skateboard bespoke.</p>
-
-                <P>Four loko Kickstarter try-hard, quinoa messenger bag banh mi twee sartorial fashion axe Brooklyn Carles cornhole banjo wolf. Intelligentsia bicycle rights gluten-free kitsch, dreamcatcher photo booth kale chips narwhal occupy pour-over DIY gentrify American Apparel 3 wolf moon. Chia Schlitz fap, YOLO Pinterest post-ironic ethical roof party. Yr wayfarers lomo synth craft beer, chambray letterpress scenester twee skateboard stumptown hashtag 3 wolf moon jean shorts. Kickstarter Banksy kitsch hoodie plaid, stumptown selvage ethical single-origin coffee quinoa asymmetrical. Single-origin coffee flexitarian before they sold out chia. Salvia banjo wolf quinoa Helvetica raw denim tousled.</P>
-
-                <P>Vinyl occupy distillery, VHS four loko lo-fi PBR. Blog semiotics 8-bit, vinyl irony kale chips photo booth sartorial brunch paleo. Bicycle rights crucifix plaid, pour-over Godard hashtag next level art party freegan meh. Cornhole Portland PBR gastropub flannel. Fanny pack wayfarers lo-fi, pork belly tattooed McSweeney's 90's you probably haven't heard of them. Ethical Wes Anderson Neutra, drinking vinegar four loko squid semiotics ennui small batch mlkshk viral chambray fashion axe. Meh Intelligentsia Marfa direct trade.</P>
-
-<P>Squid next level Banksy slow-carb banjo butcher, forage Pitchfork semiotics meh pour-over farm-to-table meggings deep v whatever. Retro Kickstarter narwhal +1, food truck mlkshk leggings Godard Shoreditch semiotics cliche. Lomo fap tattooed blog. You probably haven't heard of them church-key crucifix bicycle rights, Austin ethnic disrupt messenger bag. Sartorial gentrify retro pickled vinyl before they sold out. Disrupt Intelligentsia dreamcatcher, keffiyeh pork belly four loko flannel tattooed Thundercats vinyl Kickstarter Shoreditch kale chips banh mi bitters. Vinyl fixie banjo McSweeney's, squid shabby chic lomo pickled.</p>
-
+                <p>Place content here.</p>
             </div>
         </div>
-        <div class="row">
-            <div class="small-8 small-centered columns">
-                <div id="comment_engine">
-<h2>Comments</h2>
-<div id="ce_list">
-    
-</div>
 
-<form name="ce_form" id="ce_form" method="post" action=" ">
-    Name:<br />
-    <input type="Text" name="ce_name" id="ce_name"/>
-    <br />
-    
-    Email:<br />
-     <input type="Text" name="ce_email" id="ce_email"/>
-    <br />
-    
-    Comment:<br />
-    <textarea name="ce_comment" id="ce_comment"></textarea>
-    
-    <br />
-    
-    <input type="submit" value="Add Comment"/>
-</form>
-</div>
+        <div class="row">
+            <div class="small-8 small-centered columns blog-content">
+
+                <div id="comment_engine">
+                    <h2 class="name">So, What do you think?</h2>
+
+                    <div id="ce_list">                      
+                    </div>
+
+                    <form name="ce_form" id="ce_form" method="post" action=" ">
+
+                      <div class="row">
+                        <div class="large-12 columns">
+                      
+                            <input class="r_input" type="text" name="ce_name" id="ce_name" placeholder="Your name" />
+                          
+                        </div>
+                      </div>
+
+                      <div class="row">
+                        <div class="large-12 columns">
+                        
+                            <input class="r_input"  type="text" name="ce_email" id="ce_email" placeholder="Your email" />
+                          
+                        </div>
+                      </div>
+                      
+                      <div class="row">
+                        <div class="large-12 columns">
+                        
+                            <textarea class="r_input"  name="ce_comment" id="ce_comment" placeholder="Your comment"></textarea>
+                         
+                        </div>
+                      </div>
+
+                    <input type="submit" id="reply_submit" value="Add Comment"/>
+
+                    </form>
+                </div>
 
 <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
 <script type="text/javascript">
@@ -118,16 +137,14 @@ include "header.php";
                             ceLoadComments();
                 }});
     }
-    </script>
+</script>
+
+    
             </div>
         </div>
+
     </div>
-    
-</div>
+    </div>
 </div>
 
 <?php include 'footer.php'; ?>
-
-
-
-
--- web/prabhjot.php
+++ web/prabhjot.php
@@ -1,76 +1,101 @@
-<?php
+<?php include "header.php"; ?>
 
-include "header.php";
-
-?>
 <div class="wrapper">
+
     <div class="row">
     <div class="large-12 columns blgcontent">
-        <div class="black-ribbon">
-<!--            <img class="bookmark" src="images/bookmark.png" />-->
+        
+        <div class="row">  
+          <div class="small-12 black-ribbon">
+              <a href="index.php"><img class="bookmark" src="images/bookmark.png" /></a>
+          </div>
         </div>
+        
         <div class="row">
             <div class="small-8 small-centered columns blog-head">
-                <div class="row">
-                    <div class="small-10 columns line">
-                        <h1>REUNION ISLAND COFFEE</h1>
-                        <label class="by">By</label><label class="name"> PRABHJOT MAND</label>
+
+                <div class="row">                    
+                    <div class="small-12 medium-10 columns line">
+                        <h1>Reunion Island Coffee</h1>
+                        
+                        <p><span class="by">By</span> <span class="name">Prabhjot Mand</span></p>
+
                         <div class="divline"></div>
-                        <div class="row">
-                            <div class="medium-4 columns date-cont"><img src="images/clock.png" /><label class="date"> May 26, 2014</label></div>
-                            <div class="medium-4 columns comments-cont"><img src="images/comments.png" /><label class="comments"></label></div>
-                            <div class="medium-4 columns"></div>
+                        
+                        <div class="row">                         
+                            <div class="medium-4 columns date-cont">
+                                <img src="images/clock.png" />
+                                <span class="date"> May 26, 2014</span>
+                            </div>
+                            
+                            <div class="medium-4 columns comments-cont">
+                                <img src="images/comments.png" />
+                                <label class="comments"></label>
+                            </div>
+
                             <div class="medium-4 columns"></div>
                         </div>
-                        
                     </div>
-                    <div class="small-2 columns auth-img">
-                        
-                        <img src="images/prabhjot2.png" height="100" width="100">
-                        
+
+                    <div class="small-12 medium-2 columns auth-img">                      
+                        <img src="images/prabhjot2.png">
                     </div>
                 </div>
-                 
 
             </div>
         </div>
+
         <div class="row">
             <div class="small-8 small-centered columns blog-content">
                 <p>Reunion Island Coffee is a Canadian company that is founded on marvelous principles. It's not just about high-quality coffee - they really are stanch to sustainability, from auxiliary co-op farmers and fair profession to promoting liable farm administration. They support the Rainforest Alliance, and use Bullfrog Power in their Canadian sweltering operation. With fair trade and organic certified coffees, Reunion Island is bringing us astounding brews with zero culpability factors.</p>
 
-                <P>Out of a bag of Reunion Island Coffee, and let me tell you, it is a thorny decision on which variety to choose. They have everything from the more stock to the exotic - I tried to count the available varieties and came up with ninety-six plus twenty-six kinds of pods! Um, wow. </P>
+                <p>Out of a bag of Reunion Island Coffee, and let me tell you, it is a thorny decision on which variety to choose. They have everything from the more stock to the exotic - I tried to count the available varieties and came up with ninety-six plus twenty-six kinds of pods! Um, wow. </p>
 
-                <P>Although tempted by the gingered varieties, I went with the Siesta Decaf. It's a nice, bright, medium-light roast that is a perfect contrast to the dark and bold roast that is my regular brew. I'm a serious coffee lover - I grind fresh beans and make a cappuccino every morning with my beloved espresso machine, so I feel qualified to say that the Siesta Decaf is a lovely bean. It doesn't have the 'flat' flavour that is often found in a lighter roast; instead it has a robust flavour, just lighter. It is decaffeinated with the Swiss Water process (in British Columbia!), which uses no chemicals to remove the caffeine and also ensures the flavour stays at its fullest (some other decaffeinating methods which use chemicals also alter the flavour of the bean).</P>
+                <p>Although tempted by the gingered varieties, I went with the Siesta Decaf. It's a nice, bright, medium-light roast that is a perfect contrast to the dark and bold roast that is my regular brew. I'm a serious coffee lover - I grind fresh beans and make a cappuccino every morning with my beloved espresso machine, so I feel qualified to say that the Siesta Decaf is a lovely bean. It doesn't have the 'flat' flavour that is often found in a lighter roast; instead it has a robust flavour, just lighter. It is decaffeinated with the Swiss Water process (in British Columbia!), which uses no chemicals to remove the caffeine and also ensures the flavour stays at its fullest (some other decaffeinating methods which use chemicals also alter the flavour of the bean).</p>
 
-<P>The quality of the coffee is a major selling point, but what tips the balance with Reunion Island Coffee for me is their commitment to better lives and a better world. I was thrilled to review the experiences of the workers as well as the customers associated with such an impressive Canadian company, and would honestly recommend their products.</p>
-
+                <p>The quality of the coffee is a major selling point, but what tips the balance with Reunion Island Coffee for me is their commitment to better lives and a better world. I was thrilled to review the experiences of the workers as well as the customers associated with such an impressive Canadian company, and would honestly recommend their products.</p>
             </div>
         </div>
-        <div class="row">
-            <div class="small-8 small-centered columns">
-                <div id="comment_engine">
-<h2>Comments</h2>
-<div id="ce_list">
-    
-</div>
 
-<form name="ce_form" id="ce_form" method="post" action=" ">
-    Name:<br />
-    <input type="Text" name="ce_name" id="ce_name"/>
-    <br />
-    
-    Email:<br />
-     <input type="Text" name="ce_email" id="ce_email"/>
-    <br />
-    
-    Comment:<br />
-    <textarea name="ce_comment" id="ce_comment"></textarea>
-    
-    <br />
-    
-    <input type="submit" value="Add Comment"/>
-</form>
-</div>
+        <div class="row">
+            <div class="small-8 small-centered columns blog-content">
+
+                <div id="comment_engine">
+                    <h2 class="name">So, What do you think?</h2>
+
+                    <div id="ce_list">                      
+                    </div>
+
+                    <form name="ce_form" id="ce_form" method="post" action=" ">
+
+                      <div class="row">
+                        <div class="large-12 columns">
+                      
+                            <input class="r_input" type="text" name="ce_name" id="ce_name" placeholder="Your name" />
+                          
+                        </div>
+                      </div>
+
+                      <div class="row">
+                        <div class="large-12 columns">
+                        
+                            <input class="r_input"  type="text" name="ce_email" id="ce_email" placeholder="Your email" />
+                          
+                        </div>
+                      </div>
+                      
+                      <div class="row">
+                        <div class="large-12 columns">
+                        
+                            <textarea class="r_input"  name="ce_comment" id="ce_comment" placeholder="Your comment"></textarea>
+                         
+                        </div>
+                      </div>
+
+                    <input type="submit" id="reply_submit" value="Add Comment"/>
+
+                    </form>
+                </div>
 
 <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
 <script type="text/javascript">
@@ -118,16 +143,14 @@ include "header.php";
                             ceLoadComments();
                 }});
     }
-    </script>
+</script>
+
+    
             </div>
         </div>
+
     </div>
-    
-</div>
+    </div>
 </div>
 
 <?php include 'footer.php'; ?>
-
-
-
-
